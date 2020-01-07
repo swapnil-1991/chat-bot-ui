50c2b3f441893c2050f38a1f8cad40ac5d6e6113
Swapnil Rajput <swapnil.rajput@blackstraw.ai>
2020-01-07 14:55:08 +0530

    widget added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  addScript: boolean = false;
 
   constructor() {}
+
+  ngAfterViewChecked(): void {
+    if (!this.addScript) {
+      this.addPureChatScript();
+    }
+  }
+
+  addPureChatScript() {
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      const scripttagElement = document.createElement('script');
+      scripttagElement.type = 'text/javascript';
+      scripttagElement.innerHTML = `(function(){var js,fs,d=document,id="tars-widget-script",b="https://tars-file-upload.s3.amazonaws.com/bulb/";if(!d.getElementById(id)){js=d.createElement("script");js.id=id;js.type="text/javascript";js.src=b+"js/widget.js";fs=d.getElementsByTagName("script")[0];fs.parentNode.insertBefore(js,fs)}})();window.tarsSettings = {"convid":"Vy-wgM"};`;
+      document.getElementsByTagName('head')[0].appendChild(scripttagElement);
+    })
+  }
 
 }
